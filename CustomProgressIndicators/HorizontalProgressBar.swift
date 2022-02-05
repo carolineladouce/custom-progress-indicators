036d34a8497e0c4de7fe1763dea0990503a3dfae
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-02-04 20:27:28 -0500

    Added horizontal progress bar to MainViewController() Setup horizontalProgressBar's constraints

--- CustomProgressIndicators/HorizontalProgressBar.swift
+++ CustomProgressIndicators/HorizontalProgressBar.swift
@@ -8,13 +8,39 @@
 import UIKit
 
 class HorizontalProgressBar: UIView {
-
+    
     /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
+    let baseBarWidth: CGFloat = 300
+    let baseBarHeight: CGFloat = 50
+    let baseColor: UIColor = .lightGray
+    
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+        
+        context.setFillColor(UIColor.clear.cgColor)
+        context.fill(bounds)
+        
+        
+        // Draw base rect
+        let progressBarBaseRect = CGRect(x: 0, y: 0, width: baseBarWidth, height: baseBarHeight)
+        let progressBarPath = UIBezierPath(roundedRect: progressBarBaseRect, cornerRadius: progressBarBaseRect.height * 0.25)
+        baseColor.setFill()
+        progressBarPath.fill()
+        
+        
+        context.restoreGState()
+        
     }
-    */
-
+    
+    
 }
--- CustomProgressIndicators/MainViewController.swift
+++ CustomProgressIndicators/MainViewController.swift
@@ -8,14 +8,43 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    let horizontalProgressBar = HorizontalProgressBar()
+    let circularProgressIndicator = CircularProgressIndicator()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.\
         
         view.backgroundColor = UIColor.white
+        
+        view.addSubview(horizontalProgressBar)
+        setupHorizontalProgressBar()
+        
+        
+        self.view = view
     }
-
-
+    
+    
+    func setupHorizontalProgressBar() {
+        horizontalProgressBar.backgroundColor = UIColor.clear
+        horizontalProgressBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        let horizontalProgressBarConstraints = [
+            horizontalProgressBar.widthAnchor.constraint(equalToConstant: horizontalProgressBar.baseBarWidth),
+            horizontalProgressBar.heightAnchor.constraint(equalToConstant: horizontalProgressBar.baseBarHeight),
+            horizontalProgressBar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            horizontalProgressBar.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: view.frame.height * -0.25)
+        ]
+        
+        NSLayoutConstraint.activate(horizontalProgressBarConstraints)
+    }
+    
+    
+    func setupCircularProgressIndicator() {
+        
+        
+    }
+    
 }
 
