4591773e4e10085cda247df4f368f145c6d15143
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-02-04 19:31:14 -0500

    Added classes HorizontalProgressBar() and CircularProgressIndicator()

---
+++ CustomProgressIndicators/CircularProgressIndicator.swift
@@ -0,0 +1,20 @@
+//
+//  CircularProgressIndicator.swift
+//  CustomProgressIndicators
+//
+//  Created by Caroline LaDouce on 2/4/22.
+//
+
+import UIKit
+
+class CircularProgressIndicator: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CustomProgressIndicators/HorizontalProgressBar.swift
@@ -0,0 +1,20 @@
+//
+//  HorizontalProgressBar.swift
+//  CustomProgressIndicators
+//
+//  Created by Caroline LaDouce on 2/4/22.
+//
+
+import UIKit
+
+class HorizontalProgressBar: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
