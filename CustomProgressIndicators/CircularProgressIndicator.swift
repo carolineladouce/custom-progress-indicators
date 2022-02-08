e006ad38b15ad6489c0577a93ee70ac8b3552834
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-02-07 23:42:54 -0500

    Added progressFillLayer for CircularProgressIndicator

--- CustomProgressIndicators/CircularProgressIndicator.swift
+++ CustomProgressIndicators/CircularProgressIndicator.swift
@@ -8,17 +8,19 @@
 import UIKit
 
 class CircularProgressIndicator: UIView {
-
+    
     /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
     
     let baseCircleWidth: CGFloat = 300
     let baseCircleHeight: CGFloat = 300
+    let strokeColor: UIColor = .systemPurple
+    var progress: CGFloat = 0.5
     
     override func draw(_ rect: CGRect) {
         
@@ -30,21 +32,33 @@ class CircularProgressIndicator: UIView {
         
         context.fill(bounds)
         
-
+        
         let baseRect = CGRect(x: 0, y: 0, width: baseCircleWidth, height: baseCircleHeight)
-
+        
         // Draw circle
         let circleMask = CAShapeLayer()
         let baseCirclePath = UIBezierPath(ovalIn: baseRect.insetBy(dx: 50, dy: 50))
-
+        
         circleMask.path = baseCirclePath.cgPath
         circleMask.lineWidth = 50
         circleMask.strokeColor = UIColor.black.cgColor
         circleMask.fillColor = nil
-
+        
         layer.mask = circleMask
+        
+        let progressFillLayer = CAShapeLayer()
+        
+        progressFillLayer.path = circleMask.path
+        progressFillLayer.strokeStart = 0
+        progressFillLayer.strokeEnd = progress
+        progressFillLayer.lineWidth = circleMask.lineWidth
+        progressFillLayer.fillColor = nil
+        progressFillLayer.strokeColor = strokeColor.cgColor
+        
+        layer.addSublayer(progressFillLayer)
+        
         
         context.restoreGState()
     }
-
+    
 }
